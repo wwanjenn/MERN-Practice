8db0767c7e45ebf7d33eb4da80eb254a8d28419f
Wanjin Maglangit <wmaglangit1118@gmail.com>
2025-04-23 19:11:46 +0800

    initial frontend

---
+++ mern/client/tailwind.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,jsx,ts,tsx,html}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
