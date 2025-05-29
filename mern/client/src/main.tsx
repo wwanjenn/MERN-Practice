f069977a3e8c322660455a091395a3ee96038ab0
Wanjin Maglangit <wmaglangit1118@gmail.com>
2025-05-29 13:26:30 +0800

    ensure root exists

--- mern/client/src/main.tsx
+++ mern/client/src/main.tsx
@@ -42,8 +42,13 @@ const router = createBrowserRouter ([
   }
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const rootElement = document.getElementById("root");
+if (!rootElement) {
+  throw new Error('Root element with id "root" not found');
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <RouterProvider router= {router} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
